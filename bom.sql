806f3f0c3dc35bf13ac64b78685c2bf940d9d9b8
"Galen Church" <gchurch@shift8mfg.com>
2014-06-23 18:50:49 +0000

    qty updates

--- bom.sql
+++ bom.sql
@@ -56,21 +56,21 @@ CREATE TABLE `bom` (
 
 LOCK TABLES `bom` WRITE;
 /*!40000 ALTER TABLE `bom` DISABLE KEYS */;
-INSERT INTO `bom` VALUES (1,NULL,NULL,NULL,'Terminal Block, 35mm DIN, Gray, 26-10AWG, 32A, UT Series',36,'each',NULL,'Phoenix Contact',' 3044102','Allied',' 70169573','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169573',NULL,NULL,NULL,39.33,'purc','.95,,,,',1,NULL);
-INSERT INTO `bom` VALUES (3,NULL,NULL,NULL,'TB End Cap, UT Series',2,'each',NULL,'Phoenix Contact',' 3047028','Allied',' 70169602','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169602',NULL,NULL,NULL,1.357,'purc','.59,,,,',1,NULL);
-INSERT INTO `bom` VALUES (2,NULL,NULL,NULL,'Bridge, 2 Pos, Plug Type, UT Series',1,'each',NULL,'Phoenix Contact',' 3030161','Allied',' 70169232','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169232',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
+INSERT INTO `bom` VALUES (1,NULL,NULL,NULL,'Terminal Block, 35mm DIN, Gray, 26-10AWG, 32A, UT Series',60,'each',NULL,'Phoenix Contact',' 3044102','Allied',' 70169573','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169573',NULL,NULL,NULL,39.33,'purc','.95,,,,',1,NULL);
+INSERT INTO `bom` VALUES (3,NULL,NULL,NULL,'TB End Cap, UT Series',4,'each',NULL,'Phoenix Contact',' 3047028','Allied',' 70169602','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169602',NULL,NULL,NULL,1.357,'purc','.59,,,,',1,NULL);
+INSERT INTO `bom` VALUES (2,NULL,NULL,NULL,'Bridge, 2 Pos, Plug Type, UT Series',6,'each',NULL,'Phoenix Contact',' 3030161','Allied',' 70169232','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169232',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
 INSERT INTO `bom` VALUES (4,NULL,NULL,NULL,'DIN rail, 35mm, 2m lg.',1,'each',NULL,'Phoenix Contact',' 0801733','Allied',' 70169109','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169109',NULL,NULL,NULL,6.601,'purc','5.74,,,,',1,NULL);
-INSERT INTO `bom` VALUES (5,NULL,NULL,NULL,'TB Partician Plate, UT Series',4,'each',NULL,'Phoenix Contact',' 3047167','Allied',' 70169605','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169605',NULL,NULL,NULL,3.174,'purc','.69,,,,',1,NULL);
-INSERT INTO `bom` VALUES (6,NULL,NULL,NULL,'TB End Clamp',4,'each',NULL,'Phoenix Contact',' 0800886','Allied',' 70169101','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169101',NULL,NULL,NULL,6.578,'purc','1.43,,,,',1,NULL);
+INSERT INTO `bom` VALUES (5,NULL,NULL,NULL,'TB Partician Plate, UT Series',10,'each',NULL,'Phoenix Contact',' 3047167','Allied',' 70169605','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169605',NULL,NULL,NULL,3.174,'purc','.69,,,,',1,NULL);
+INSERT INTO `bom` VALUES (6,NULL,NULL,NULL,'TB End Clamp',6,'each',NULL,'Phoenix Contact',' 0800886','Allied',' 70169101','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169101',NULL,NULL,NULL,6.578,'purc','1.43,,,,',1,NULL);
 INSERT INTO `bom` VALUES (7,NULL,NULL,NULL,'PLC, LOGO ',1,'each',NULL,'Siemens','6ED1052-1MD00-0BA7',NULL,NULL,'http://www.alliedelec.com/search/productdetail.aspx?SKU=70241223&utm_source=octopart&utm_medium=part_sourcing&utm_campaign=octopart',NULL,NULL,NULL,217.684,'purc','189.29,,,,',1,NULL);
 INSERT INTO `bom` VALUES (8,NULL,NULL,NULL,'I/O Expansion, LOGO, 4OUT',2,'each',NULL,'Siemens','6ED1055-1MB00-0BA1','Allied','70240432','http://www.alliedelec.com/search/productdetail.aspx?SKU=70240432&utm_source=octopart&utm_medium=part_sourcing&utm_campaign=octopart#tab=overview',NULL,NULL,NULL,186.806,'purc','81.22,,,,',1,NULL);
 INSERT INTO `bom` VALUES (9,NULL,NULL,NULL,'Power Supply, 24VDC, 4A, LOGO, DIN',1,'each',NULL,'Siemens','6EP13321SH52',NULL,NULL,'http://www.alliedelec.com/search/productdetail.aspx?SKU=70240446#tab=overview',NULL,NULL,NULL,94.461,'purc','82.14,,,,',1,NULL);
 INSERT INTO `bom` VALUES (10,NULL,NULL,NULL,'Power Supply, 24VDC, 5A, Mean Well',1,'each',NULL,'MeanWell','DR-120-24','Allied','70069749','http://www.alliedelec.com/search/productdetail.aspx?SKU=70069749#tab=specs',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
 INSERT INTO `bom` VALUES (11,NULL,NULL,NULL,'Enclosure, Box, 14x12x6, NEMA12',1,'each',NULL,'Hoffman','A1412CH','Allied','70066815','http://www.alliedelec.com/search/productdetail.aspx?SKU=70066815#tab=specs',NULL,NULL,NULL,160.241,'purc','139.34,,,,',1,NULL);
 INSERT INTO `bom` VALUES (12,NULL,NULL,NULL,'Enclosure, Panel, ~12x10, Steel',1,'each',NULL,'Hoffman','A14P12','Allied','70066696','http://www.alliedelec.com/search/productdetail.aspx?SKU=70066696',NULL,NULL,NULL,17.664,'purc','15.36,,,,',1,NULL);
-INSERT INTO `bom` VALUES (13,NULL,NULL,NULL,'Wire, 18AWG, 300V, PVC, Blk',1,'each',NULL,'Alpha Wire','3055 BK005','Allied','70136442','http://www.alliedelec.com/search/productdetail.aspx?SKU=70136442',NULL,'100ft coil',NULL,48.2195,'purc','41.93,,,,',1,NULL);
+INSERT INTO `bom` VALUES (13,NULL,NULL,NULL,'Wire, 18AWG, 300V, PVC, Blu',1,'each',NULL,'Alpha Wire','3055 BL005','Allied','70136446','http://www.alliedelec.com/search/productdetail.aspx?SKU=70136446',NULL,'100ft coil',NULL,48.2195,'purc','41.93,,,,',1,NULL);
 INSERT INTO `bom` VALUES (14,NULL,NULL,NULL,'Wire, 18AWG, 300V, PVC, Wht',1,'each',NULL,'Alpha Wire','3055 WH005','Allied','70136441','http://www.alliedelec.com/search/productdetail.aspx?SKU=70136441',NULL,'100ft coil',NULL,48.2195,'purc','41.93,,,,',1,NULL);
-INSERT INTO `bom` VALUES (15,NULL,NULL,NULL,'Wire, 18AWG, 300V, PVC, Vlt',1,'each',NULL,'Alpha Wire','3055 VI005','Allied','70136440',NULL,NULL,'100ft coil',NULL,48.2195,'purc','41.93,,,,',1,NULL);
+INSERT INTO `bom` VALUES (15,NULL,NULL,NULL,'Wire, 22AWG, 300V, PVC, Vlt',1,'each',NULL,'Alpha Wire','3051 VI005','Allied','70136420','http://www.alliedelec.com/search/productdetail.aspx?SKU=70136420',NULL,'100ft coil',NULL,48.2195,'purc','41.93,,,,',1,NULL);
 INSERT INTO `bom` VALUES (16,NULL,NULL,NULL,'Software, LOGO',1,'each',NULL,'Siemens','6ED10580BA020YA1','Allied','70240444','http://www.alliedelec.com/search/productdetail.aspx?SKU=70240444',NULL,'OTP',NULL,85.2955,'purc','74.17,,,,',1,NULL);
 INSERT INTO `bom` VALUES (17,NULL,NULL,NULL,'Terminal Block, 35mm DIN, Ground, yel/grn, UT 4 series',3,'each',NULL,'Phoenix Contact','3044128','Allied','70169579','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169579',NULL,NULL,NULL,14.628,'purc','4.24,,,,',1,NULL);
 INSERT INTO `bom` VALUES (18,NULL,NULL,NULL,'HMI, LOGO TD, 4-line text',1,'each',NULL,'Siemens','6ED1 055-4MH00-0BA0','Control Associates','70240435','http://www.alliedelec.com/search/productdetail.aspx?SKU=70240435',NULL,NULL,NULL,151.8,'purc','132,,,,',1,NULL);
@@ -89,7 +89,7 @@ INSERT INTO `bom` VALUES (30,NULL,NULL,NULL,'Conn, DB9, Female',1,'each',NULL,'T
 INSERT INTO `bom` VALUES (31,NULL,NULL,NULL,'Cable Gland, PG11',4,'each',NULL,'Lapp','S2111','Allied','70123697','http://www.alliedelec.com/search/productdetail.aspx?SKU=70123697#tab=overview',NULL,NULL,NULL,4.968,'purc','1.08,,,,',1,NULL);
 INSERT INTO `bom` VALUES (32,'SHEET01',NULL,NULL,'Knockout Panel',1,'each',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
 INSERT INTO `bom` VALUES (33,NULL,NULL,NULL,'Cable, DB25, 10FT, M/F',1,'each',NULL,'L-COM`','CS2N25MF-10','Allied','70126163','http://www.alliedelec.com/search/productdetail.aspx?SKU=70126163#tab=specs',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
-INSERT INTO `bom` VALUES (34,NULL,NULL,NULL,'Cable, DB9, 25FT, M/F',1,'each',NULL,'L-COM','CSM9MF-25','Allied','70126553','http://www.alliedelec.com/search/productdetail.aspx?SKU=70126553#tab=specs',NULL,'For Pendant',NULL,NULL,NULL,NULL,1,NULL);
+INSERT INTO `bom` VALUES (34,NULL,NULL,NULL,'Cable, DB9, 25FT, M/F',1,'each',NULL,'L-COM','CRMN9MF-25','Allied','70126231','http://www.alliedelec.com/search/productdetail.aspx?SKU=70126231#tab=specs',NULL,'For Pendant',NULL,NULL,NULL,NULL,1,NULL);
 /*!40000 ALTER TABLE `bom` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -102,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-23 16:07:20
+-- Dump completed on 2014-06-23 18:50:49
