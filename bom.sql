902c3333326161d84c45449ccd9cf734ec9b066c
"Galen Church" <gchurch@shift8mfg.com>
2014-06-24 13:25:15 +0000

    dsub hardware

--- bom.sql
+++ bom.sql
@@ -47,7 +47,7 @@ CREATE TABLE `bom` (
   PRIMARY KEY (`item`),
   KEY `item` (`item`),
   KEY `part` (`part`)
-) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,14 +75,14 @@ INSERT INTO `bom` VALUES (16,NULL,NULL,NULL,'Software, LOGO',1,'each',NULL,'Siem
 INSERT INTO `bom` VALUES (17,NULL,NULL,NULL,'Terminal Block, 35mm DIN, Ground, yel/grn, UT 4 series',3,'each',NULL,'Phoenix Contact','3044128','Allied','70169579','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169579',NULL,NULL,NULL,14.628,'purc','4.24,,,,',1,NULL);
 INSERT INTO `bom` VALUES (18,NULL,NULL,NULL,'HMI, LOGO TD, 4-line text',1,'each',NULL,'Siemens','6ED1 055-4MH00-0BA0','Control Associates','70240435','http://www.alliedelec.com/search/productdetail.aspx?SKU=70240435',NULL,NULL,NULL,151.8,'purc','132,,,,',1,NULL);
 INSERT INTO `bom` VALUES (19,NULL,NULL,NULL,'Relay, DPDT, 6A, DIN, 24VDC Ctrl',1,'each',NULL,'Phoenix Contact','2967060','Allied','70207723','http://www.alliedelec.com/search/productdetail.aspx?SKU=70207723&utm_source=octopart&utm_medium=part_sourcing&utm_campaign=octopart',NULL,NULL,NULL,30.0725,'purc','26.15,,,,',1,NULL);
-INSERT INTO `bom` VALUES (20,NULL,NULL,NULL,'Switch, Lever',1,'each',NULL,'Omron','V15G31C25KBYOMI','Allied','70175254','http://www.alliedelec.com/search/productdetail.aspx?SKU=70175254&utm_source=octopart&utm_medium=part_sourcing&utm_campaign=octopart',NULL,'100,000 cycles',NULL,2.9785,'purc','2.59,,,,',1,NULL);
+INSERT INTO `bom` VALUES (20,NULL,NULL,NULL,'Switch, Lever',1,'each',NULL,'Omron',' V15G31C26KBYOMI','Allied',' 70175764','http://www.alliedelec.com/search/productdetail.aspx?SKU=70175764',NULL,'100,000 cycles',NULL,2.9785,'purc','2.59,,,,',1,NULL);
 INSERT INTO `bom` VALUES (21,NULL,NULL,NULL,'Fuse Holder, DIN, 24V LED',3,'each',NULL,'Phoenix Contact','3046090','Allied','70169590','http://www.alliedelec.com/search/productdetail.aspx?SKU=70169590&utm_source=octopart&utm_medium=part_sourcing&utm_campaign=octopart#tab=specs',NULL,NULL,NULL,30.222,'purc','8.76,,,,',1,NULL);
 INSERT INTO `bom` VALUES (22,NULL,NULL,NULL,'Fuse, 0.5A, 5x20mm',3,'each',NULL,'Schurter','0034.3114','Allied','70159925','http://www.alliedelec.com/search/productdetail.aspx?SKU=70159925#tab=specs',NULL,NULL,NULL,1.3455,'purc','.39,,,,',1,NULL);
 INSERT INTO `bom` VALUES (23,NULL,NULL,NULL,'Fuse, 2A, 5x20mm',3,'each',NULL,'Schurter','0001.2507','Allied','70160339','http://www.alliedelec.com/search/productdetail.aspx?SKU=70160339',NULL,NULL,NULL,1.2765,'purc','.37,,,,',1,NULL);
 INSERT INTO `bom` VALUES (24,NULL,NULL,NULL,'Fuse, 0.8A, 5x20mm',3,'each',NULL,'Schurter','0034.3116','Allied','70159929','http://www.alliedelec.com/search/productdetail.aspx?SKU=70159929',NULL,NULL,NULL,1.4835,'purc','.43,,,,',1,NULL);
 INSERT INTO `bom` VALUES (25,NULL,NULL,NULL,'Switch, EMO',1,'each',NULL,'IDEC','HW1B-V4F01-R','Allied','70174988','http://www.alliedelec.com/search/productdetail.aspx?SKU=70174988&utm_source=octopart&utm_medium=part_sourcing&utm_campaign=octopart',NULL,NULL,NULL,36.708,'purc','31.92,,,,',1,NULL);
 INSERT INTO `bom` VALUES (26,NULL,NULL,NULL,'Pendant, 2 Button',1,'each',NULL,'Schneider Electric',' XALD211H29H7','Allied','70231192','http://www.alliedelec.com/search/productdetail.aspx?SKU=70231192&utm_source=octopart&utm_medium=part_sourcing&utm_campaign=octopart#tab=specs',NULL,NULL,NULL,83.5935,'purc','72.69,,,,',1,NULL);
-INSERT INTO `bom` VALUES (27,NULL,NULL,NULL,'Cable Gland, PG-13.5',2,'each',NULL,'Lapp','S2113','Allied','70123698','http://www.alliedelec.com/search/productdetail.aspx?SKU=70123698',NULL,'For pendant',NULL,NULL,NULL,NULL,1,NULL);
+INSERT INTO `bom` VALUES (27,NULL,NULL,NULL,'Cable Gland, PG-13.5',1,'each',NULL,'Lapp','S2113','Allied','70123698','http://www.alliedelec.com/search/productdetail.aspx?SKU=70123698',NULL,'For pendant',NULL,NULL,NULL,NULL,1,NULL);
 INSERT INTO `bom` VALUES (28,NULL,NULL,NULL,'Fan, Muffin, 60mm, 24VDC',1,'each',NULL,'McMaster-Carr','1939K59','McMaster-Carr','1939K59','http://www.mcmaster.com/#1939k59/=sc5ncs',NULL,NULL,NULL,27.1055,'purc','23.57,,,,',1,NULL);
 INSERT INTO `bom` VALUES (29,NULL,NULL,NULL,'Conn, DB25, Female',1,'each',NULL,'TE','5-747913-2','Allied','70041630','http://www.alliedelec.com/search/productdetail.aspx?SKU=70041630',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
 INSERT INTO `bom` VALUES (30,NULL,NULL,NULL,'Conn, DB9, Female',1,'each',NULL,'TE','5-747905-7','Allied','70084893','http://www.alliedelec.com/search/productdetail.aspx?SKU=70084893',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
@@ -106,6 +106,7 @@ INSERT INTO `bom` VALUES (47,NULL,NULL,NULL,'Drill Bit, 6mm, B-G Oxide',1,'each'
 INSERT INTO `bom` VALUES (48,NULL,NULL,NULL,'BHCS, M4, 35mm, 18-8 SS, 25pk',1,'each',NULL,NULL,NULL,'McMaster-Carr','92095A199','http://www.mcmaster.com/#92095a199/=sjgrnp',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
 INSERT INTO `bom` VALUES (49,NULL,NULL,NULL,'Hex Nut, M4, SS',1,'each',NULL,NULL,NULL,'McMaster-Carr','94150A335','http://www.mcmaster.com/#94150a335/=sjgpwk',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
 INSERT INTO `bom` VALUES (50,NULL,NULL,NULL,'BHCS, M4, 12mm, SS',1,'each',NULL,NULL,NULL,'McMaster-Carr','94500A228','http://www.mcmaster.com/#94500a228/=sjgx68',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
+INSERT INTO `bom` VALUES (51,NULL,NULL,NULL,'Hardware, DSUB, 4-40',2,'each',NULL,'TE','5205817-3','Allied','70043051','http://www.alliedelec.com/search/productdetail.aspx?SKU=70043051',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
 /*!40000 ALTER TABLE `bom` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -118,4 +119,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-24  2:12:30
+-- Dump completed on 2014-06-24 13:25:15
